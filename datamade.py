f0780be439bfb9fa1bb2bd80d5a8ecd727d57d3f
Your Name <you@example.com>
2019-11-15 21:49:32 -0600

    Code for spreadsheet manipulation

--- datamade.py
+++ datamade.py
@@ -1,37 +1,80 @@
-import csv, datetime
+import csv, datetime 
 from datetime import timedelta
 
-with open('legislators.csv', 'r') as csv_file:
-    csv_reader = csv.reader(csv_file)
+def csv_data():
+    csv_object = {} #variable will ensure that data from the csv file is held in an object
+    list_of_csv_objects = [] #variable holds all objects as list items
+    with open('legislators.csv','r') as csv_file: 
+        csv_object = csv.DictReader(csv_file) #DictReader reads first line of the CSV and using each comma separated value in every line a dictionary key.
 
-    next(csv_reader)
-
-    # d = datetime.date.today()
-    # print(type(d))
-    # print(d)
-
-    # x = datetime.date(2020, 5, 17)
-    # fixeddate = datetime.datetime.year(1974,4,11)
-    # # z = 
-    # y = fixeddate 
-    # print(y)
-
-
-    for line in csv_reader:
-        tday = datetime.date.today()
+        for line in csv_object:
+            new_object = {}
+            new_object.update(line)
+            list_of_csv_objects.append(new_object)
     
-        y = tday 
+    return list_of_csv_objects
+
+def filter_age_45():
+    list_of_people = csv_data() #gives me list of people as objects
+
+
+    younger_than_45_list = []
+    for person in list_of_people:
+        tday = datetime.date.today()
         fourtyfive = datetime.timedelta(16435,00,00)
-        date_time_str = line[27]
+        date_time_str = person['birthdate']
         date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d')
-        fourtyfivetest = y - date_time_obj.date()
+        calculation_of_age = tday - date_time_obj.date()
 
-        if fourtyfivetest <= fourtyfive:
-            print("45 or younger")
+        if calculation_of_age < fourtyfive:
+            younger_than_45_list.append(person)
+
+    return younger_than_45_list
+
+def filter_republicans_social_media():
+    list_of_people = csv_data() #gives me list of people as objects
+
+    social_republicans_list = []
+    for person in list_of_people:
+
+        if person['party'] == 'R' and person['twitter_id'] != '' and person['youtube_url'] != '':
+            social_republicans_list.append(person)
+    return social_republicans_list
+filter_republicans_social_media()
+
+def write_to_new_csv_file():
+    younger_than_45_list = filter_age_45()
+    csv_headers = younger_than_45_list[0].keys()
+    csv_headers_list = []
+
+    for header_name in csv_headers:
+        csv_headers_list.append(header_name)
+
+    with open('45younger.csv','w')as new_file:
+        csv_writer = csv.DictWriter(new_file, fieldnames = csv_headers_list)
+        csv_writer.writeheader()
+
+        for person in younger_than_45_list:
+            csv_writer.writerow(person)
 
 
-        print(type(date_time_obj.date()))
+def write_to_new_csv_file2():
+    republicans_socialmedia_list = filter_republicans_social_media()
+    csv_headers = republicans_socialmedia_list[0].keys()
+    csv_headers_list = []
 
-        print(fourtyfivetest)
+    for header_name in csv_headers:
+        csv_headers_list.append(header_name)
 
-        print(fourtyfive)
+    with open('social_Republicans.csv','w')as new_file:
+        csv_writer = csv.DictWriter(new_file, fieldnames = csv_headers_list)
+        csv_writer.writeheader()
+
+        for person in republicans_socialmedia_list:
+            csv_writer.writerow(person)
+
+def run():
+    write_to_new_csv_file()
+    write_to_new_csv_file2()
+
+run()
